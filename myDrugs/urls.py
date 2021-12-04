28a9c0f9a5202579675c52d95ea40c772071557e
oslunt <77417770+oslunt@users.noreply.github.com>
2021-12-04 15:31:01 -0700

    Pain

--- myDrugs/urls.py
+++ myDrugs/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import indexPageView, searchPageView, detailsPageView, recordsPageView, analysisPageView, showSingleRecordPageView, updatePrescribersPageView
+from .views import indexPageView, searchPageView, detailsPageView, recordsPageView, analysisPageView, showSingleRecordPageView, updatePrescribersPageView, prescriberSearchPageView, drugSearchPageView, detailsDrugPageView
 
 urlpatterns = [
+    path("search/prescriber/<int:prescriber>", detailsPageView, name="detailsPrescriber"),
+    path("search/prescriber/", prescriberSearchPageView, name="prescriber"),
+    path("search/drug/<int:drug>", detailsDrugPageView, name="detailsDrug"),
+    path("search/drug/", drugSearchPageView, name="drug"),
     path("search/", searchPageView, name="search"),
     path("details/", detailsPageView, name="details"),
     path("analysis/", analysisPageView, name="analysis"),
@@ -9,4 +13,4 @@ urlpatterns = [
     path("showSingleRecord/<int:pres_id>", showSingleRecordPageView, name="showSingleRecord"),
     path("updatePrescribers/", updatePrescribersPageView, name="updatePres"),
     path("", indexPageView, name="index"),
-]
+] 
